dcabd4460c76262401e7d79d45c3f139dc215150
giuliagrfo <giuliagrifo1@gmail.com>
2023-01-19 16:46:05 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    components: {
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <section class="vue-home">
+        <div class="container">
+            <h1>Projects</h1>
+            <div class="row">
+                <div class="col-12 d-flex flex-column justify-content-center align-items-center vh-100">
+
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss';
+</style>
