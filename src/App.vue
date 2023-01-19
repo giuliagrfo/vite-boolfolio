5cf83343cf51833a8156662e94f8fec53c79ec49
giuliagrfo <giuliagrifo1@gmail.com>
2023-01-19 17:31:06 +0100

    Return data

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 export default {
     components: {
     },
     data() {
         return {
-
+            projects: null,
+            base_api_url: 'http://localhost:8000',
+            loading: true
         }
+
+    },
+    methods: {
+        getProjects(url) {
+            // ajax call
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(response.data);
+                    this.projects = response.data;
+                })
+                .catch(error => {
+                    console.error(error.message);
+                    this.error = error.message;
+                })
+        }
+    },
+    mounted() {
+        this.getProjects(this.base_api_url + '/api/projects');
     }
 }
 </script>
@@ -16,8 +37,13 @@ export default {
     <section class="vue-home">
         <div class="container">
             <h1>Projects</h1>
-            <div class="row">
-                <div class="col-12 d-flex flex-column justify-content-center align-items-center vh-100">
+            <div class="row" v-if="!loading">
+                <div class="col-3" v-for="project in projects.data">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4>{{ project.title }}</h4>
+                        </div>
+                    </div>
 
                 </div>
             </div>
