ccf1779f8b63915d72180e00ae3d1732d7ffbca2
giuliagrfo <giuliagrifo1@gmail.com>
2023-01-25 18:29:51 +0100

    fix form

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -57,25 +57,19 @@ export default {
         <h1 class="text-uppercase">Contacts</h1>
 
         <div v-if="success" class="alert alert-success" role="alert">
-            Messaggio inviato con successo
+            Messaggio inviato con successo!
         </div>
 
         <form @submit.prevent="sendForm()">
             <div class="mb-3">
-                <label for="name" class="form-label">Name</label>
-                <input type="text" name="name" id="name" v-model="name" class="form-control" placeholder="name"
+                <label for="name" class="form-label">Full Name</label>
+                <input type="text" name="name" id="name" v-model="name" class="form-control" placeholder="full name"
                     aria-describedby="fullNameHelper">
                 <p v-for="(error) in errors.name" class="alert alert-danger mt-2">
                     {{ error }}
                 </p>
-                <small id="fullNameHelper" class="text-muted">Add your name</small>
+                <small id="fullNameHelper" class="text-muted">Add your full name</small>
             </div>
-            <!-- <div class="mb-3">
-                <label for="" class="form-label">Last Name</label>
-                <input type="text" name="" id="" class="form-control" placeholder="last name"
-                    aria-describedby="fullNameHelper">
-                <small id="fullNameHelper" class="text-muted">Add your last name</small>
-            </div> -->
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" name="email" id="email" v-model="email" class="form-control"
@@ -88,7 +82,8 @@ export default {
 
             <div class="mb-3">
                 <label for="message" class="form-label">Message</label>
-                <textarea class="form-control" name="message" id="message" v-model="message" rows="5"></textarea>
+                <textarea class="form-control" name="message" id="message" v-model="message" rows="5"
+                    placeholder="type here..."></textarea>
                 <p v-for="(error) in errors.message" class="alert alert-danger mt-2">
                     {{ error }}
                 </p>
